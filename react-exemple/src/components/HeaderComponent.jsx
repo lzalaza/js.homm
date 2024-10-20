41bd751db1400823dd2edf416771f07e9207bf5f
lzalaza <lzalaza86@gmail.com>
2024-10-20 13:28:23 +0200

    Initial commit

---
+++ react-exemple/src/components/HeaderComponent.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+const HeaderComponent = () => {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+
+    const updateCount = () =>{
+        const newCount = count + 2;
+        setCount(newCount);
+
+    }
+    
+    const updateCoun = () =>{
+        const secondCount = count - 2;
+        setCount(secondCount);
+    }
+    
+    useEffect(()=>{
+        setName('Jonh')
+        console.log('happen')
+    },[])
+
+    useEffect(()=>{
+        
+    },[count])
+
+    return (
+        <div className='headerComponent'>
+            <p>Count: {count}</p>
+            <button className='addBtn' onClick={() => updateCount()}>Incrase</button>
+            <button className='addBtn' onClick={() => updateCoun()}>Decrase</button>
+            <p>{name}</p>
+        </div>
+    )
+
+
+
+     
+}
+
+
+
+export default HeaderComponent;
